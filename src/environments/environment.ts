e180b541632aa7252a287936c328dd756e8de221
michelnovellino <michelnovellino.programador@gmail.com>
2023-07-19 09:06:31 -0400

    MASF-71

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
+   export const environment = {
   production: false,
   API_URL: "https://api.masfadu.com",
   firebaseConfig: {
@@ -14,8 +14,21 @@ export const environment = {
     appId: "1:58870024888:web:cc03f8f4313765ec08424d",
     measurementId: "G-1TSRDZQXF6"
   }
-};
+};   
 
+/*   export const environment = {
+  production: false,
+  API_URL: "http://192.168.0.110:3000",
+  firebaseConfig: {
+    apiKey: "AIzaSyBDEIpAX7mXUJDm1Xx2zW0ll58A0CgUDZE",
+    authDomain: "fadu-1c40d.firebaseapp.com",
+    projectId: "fadu-1c40d",
+    storageBucket: "fadu-1c40d.appspot.com",
+    messagingSenderId: "58870024888",
+    appId: "1:58870024888:web:cc03f8f4313765ec08424d",
+    measurementId: "G-1TSRDZQXF6"
+  }
+};  */
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
