44cb1f6fee61f59524c7f23186a1e0b3c63f754c
carlosaraqueZA <caraque@zonamerica.com>
2023-12-06 01:11:59 -0300

    corrections

--- src/app/layouts/dash-layout/dash-layout.routing.ts
+++ src/app/layouts/dash-layout/dash-layout.routing.ts
@@ -8,11 +8,11 @@ export const DashLayoutRoutes: Routes = [
     { path: 'ofertas', loadChildren: () => import('../../pages/offers/offers.module').then(m => m.OffersModule) },
     { path: 'cursoss&Workshops', loadChildren: () => import('../../pages/courses/courses.module').then(m => m.CoursesModule) },
     { path: 'opiniones', loadChildren: () => import('../../pages/opinions/opinions.module').then(m => m.OpinionsModule) },
-    { path: 'balance', loadChildren: () => import('../../pages/balance/balance.module').then(m => m.BalanceModule) },
+    // { path: 'balance', loadChildren: () => import('../../pages/balance/balance.module').then(m => m.BalanceModule) },
     { path: 'ofertas-laborales', loadChildren: () => import('../../pages/job-offers/job-offers.module').then(m => m.JobOffersModule) },
     { path: 'recursos', loadChildren: () => import('../../pages/resources/resources.module').then(m => m.ResourcesModule) },
     { path: 'noticias', loadChildren: () => import('../../pages/notice/notice.module').then(m => m.NoticeModule) },
     { path: 'anuncios', loadChildren: () => import('../../pages/ad-page/ad-page.module').then(m => m.AdPageModule) },
-    { path: 'objestos-perdidos', loadChildren: () => import('../../pages/lost-object/lost-object.module').then(m => m.LostObjectModule) },
+    // { path: 'objestos-perdidos', loadChildren: () => import('../../pages/lost-object/lost-object.module').then(m => m.LostObjectModule) },
     // { path: 'configuraciones', loadChildren: () => import('../../pages/settings/settings.module').then(m => m.SettingsModule) },
 ];
