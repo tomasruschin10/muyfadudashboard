66aafc3044077bef42e5d4a312d3b52417b4a26f
Elvis Garcia <elviserikvan@gmail.com>
2025-01-30 11:44:32 -0400

    Simple fix test

--- src/app/layouts/privacy-policy-layout/privacy-policy-layout.component.ts
+++ src/app/layouts/privacy-policy-layout/privacy-policy-layout.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-privacy-policy-layout',
@@ -9,7 +10,8 @@ import { CommonModule } from '@angular/common';
 })
 export class PrivacyPolicyLayoutComponent {
   title = 'front-osmos';
-  externalUrl: string = 'https://tomsr13.sg-host.com/privacy-policy/';
+  // externalUrl: string = 'https://tomsr13.sg-host.com/privacy-policy/';
+  externalUrl: string = environment.API_URL;
   safeExternalUrl: SafeResourceUrl;
 
   constructor(private sanitizer: DomSanitizer) {
