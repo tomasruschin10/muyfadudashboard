490d8551f2786065c5cc71c1e80dd151e00a4a8d
Wilhelm Flores <wiljoseflores@gmail.com>
2025-06-26 10:30:11 -0400

    feat: add export to opinions

--- src/app/pages/opinions/services/opinions.service.ts
+++ src/app/pages/opinions/services/opinions.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Opinion } from 'src/app/shared/models/opinion.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +14,59 @@ export class OpinionsService {
     this.BASE_URL = environment.API_URL
   }
 
-  getOpinions(offset, limit, subject_id, tagsId, search, career_id){
-    return this.http.get(`${this.BASE_URL}/opinion/all?offset=${offset}&limit=${limit}&career_id=${career_id}${subject_id}${tagsId}${search}`, {
+  getOpinions(offset, limit, subject_id, tagsId, search, career_id, startDate?: string, endDate?: string){
+    let params = new HttpParams()
+      .set('offset', offset.toString())
+      .set('limit', limit.toString())
+
+    if(career_id) {
+      params = params.set('career_id', career_id);
+    }
+    if(subject_id) {
+      params = params.set('subject_id', subject_id);
+    }
+    if (startDate && endDate) {
+      params = params.set('startDate', startDate);
+      params = params.set('endDate', endDate);
+    }
+    if(search) {
+      params = params.set('search', search);
+    }
+    if(tagsId && Array.isArray(tagsId)) {
+      tagsId.forEach(tagId => {
+        params = params.append('tags[]', tagId.toString());
+      });
+    }
+    return this.http.get(`${this.BASE_URL}/opinion/all`, {
+      params,
       observe: 'response'
     })
+  }
+
+  getOpinionsForExport(subject_id, tagsId, search, career_id, startDate?: string, endDate?: string){
+    let params = new HttpParams()
+
+    if(career_id) {
+      params = params.set('career_id', career_id);
+    }
+    if(subject_id) {
+      params = params.set('subject_id', subject_id);
+    }
+    if (startDate && endDate) {
+      params = params.set('startDate', startDate);
+      params = params.set('endDate', endDate);
+    }
+    if(search) {
+      params = params.set('search', search);
+    }
+    if(tagsId && Array.isArray(tagsId)) {
+      tagsId.forEach(tagId => {
+        params = params.append('tags[]', tagId.toString());
+      });
+    }
+    return this.http.get<Opinion[]>(`${this.BASE_URL}/opinion/all/export`, { params }).pipe(
+      map(response => response || null)
+    )
   }
   deleteOpinion(id){
     return this.http.delete(`${this.BASE_URL}/opinion/delete/${id}`, {
